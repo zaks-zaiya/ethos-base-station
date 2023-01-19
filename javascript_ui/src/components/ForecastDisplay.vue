a2d38a81864732ae629edce847ae507b8c857839
Connor Forbes <connorforbes25@gmail.com>
2023-01-19 15:41:26 +1000

    feat: Error handling when weather forecast is offline

--- javascript_ui/src/components/ForecastDisplay.vue
+++ javascript_ui/src/components/ForecastDisplay.vue
@@ -4,21 +4,28 @@
       <div class="text-h6">Weather Forecast</div>
 
       <q-separator inset />
-      <div><img :src="iconUrl" alt="Weather icon" /></div>
-      <div>{{ weatherDescription }}</div>
-      <div>{{ currentTemp }}°C</div>
-      <div>{{ currentHumidity }}% RH</div>
+      <div v-if="errorMessage">
+        Weather Forecast Offline <br />
+        Reason: {{ errorMessage }}
+      </div>
+      <div v-else>
+        <div><img :src="iconUrl" alt="Weather icon" /></div>
+        <div>{{ weatherDescription }}</div>
+        <div>{{ currentTemp }}°C</div>
+        <div>{{ currentHumidity }}% RH</div>
+      </div>
     </q-card-section>
   </q-card>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, computed, onMounted, onUnmounted } from 'vue';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 export default defineComponent({
   name: 'ForecastDisplay',
   setup() {
+    let errorMessage = ref('');
     let currentTemp = ref(null);
     let currentHumidity = ref(null);
     let maxTemp = ref(null);
@@ -50,22 +57,34 @@ export default defineComponent({
 
     let updateWeather = async () => {
       console.log('Updating weather...');
-      let url = `https://api.openweathermap.org/data/2.5/weather?lat=-28.0167&lon=153.400&units=metric&appid=${process.env.OPENWEATHERMAPSAPIKEY}`;
-      let result = await axios.get(url, {
-        timeout: 4000,
-        responseType: 'text',
-        maxContentLength: 65536,
-      });
-      let weatherObj = JSON.parse(result.data);
-      currentTemp.value = weatherObj.main.temp;
-      currentHumidity.value = weatherObj.main.humidity;
-      minTemp.value = weatherObj.main.temp_min;
-      maxTemp.value = weatherObj.main.temp_max;
-      weatherDescription.value = weatherObj.weather[0].description;
-      weatherIcon.value = weatherObj.weather[0].icon;
+      try {
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=-28.0167&lon=153.400&units=metric&appid=${process.env.OPENWEATHERMAPSAPIKEY}`;
+        let result = await axios.get(url, {
+          timeout: 4000,
+          responseType: 'text',
+          maxContentLength: 65536,
+        });
+        // Update weather values
+        let weatherObj = JSON.parse(result.data);
+        currentTemp.value = weatherObj.main.temp;
+        currentHumidity.value = weatherObj.main.humidity;
+        minTemp.value = weatherObj.main.temp_min;
+        maxTemp.value = weatherObj.main.temp_max;
+        weatherDescription.value = weatherObj.weather[0].description;
+        weatherIcon.value = weatherObj.weather[0].icon;
+        // Clear error message
+        errorMessage.value = '';
+      } catch (error: unknown) {
+        if (error instanceof AxiosError) {
+          errorMessage.value = error.message;
+        } else {
+          errorMessage.value = 'An unknown error occurred';
+        }
+      }
     };
 
     return {
+      errorMessage,
       currentTemp,
       currentHumidity,
       maxTemp,
