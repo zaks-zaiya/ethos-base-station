a11e7c61bf71b33bcda98dfb2b9911353ddefafe
Connor Forbes <connorforbes25@gmail.com>
2023-01-25 12:28:38 +1000

    fix: Allow all origins

--- python_radio/src/main.py
+++ python_radio/src/main.py
@@ -2,7 +2,7 @@ import eventlet
 import socketio
 
 # Allow web (9000) or electron (9300) apps to connect
-sio = socketio.Server(cors_allowed_origins=['http://localhost:9000', 'http://localhost:9300'])
+sio = socketio.Server(cors_allowed_origins='*')
 app = socketio.WSGIApp(sio)
 
 @sio.event
