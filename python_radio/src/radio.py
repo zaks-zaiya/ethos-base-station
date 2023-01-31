2814eb0dba7ae3e299e3fddd058956778a05ef48
Connor Forbes <connorforbes25@gmail.com>
2023-01-31 15:58:42 +1000

    refactor: Improved layout of code

--- python_radio/src/radio.py
+++ python_radio/src/radio.py
@@ -5,11 +5,8 @@ from digitalio import DigitalInOut
 import adafruit_rfm9x
 
 def radio_listen(sio):
-  # Define radio parameters.
-  RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
-  # module! Can be a value like 915.0, 433.0, etc.
-
   # Configure LoRa Radio
+  RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz
   CS = DigitalInOut(board.CE1)
   RESET = DigitalInOut(board.D25)
   spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
@@ -18,22 +15,16 @@ def radio_listen(sio):
 
   # Radio listen loop
   while True:
-    packet = rfm9x.receive()
-    # Optionally change the receive timeout from its default of 0.5 seconds:
-    # packet = rfm9x.receive(timeout=5.0)
+    packet = rfm9x.receive(timeout=5.0)
     # If no packet was received during the timeout then None is returned.
     if packet is not None:
       # Received a packet!
       # Print out the raw bytes of the packet:
       print("Received (raw bytes): {0}".format(packet))
-      # And decode to ASCII text and print it too.  Note that you always
-      # receive raw bytes and need to convert to a text format like ASCII
-      # if you intend to do string processing on your data.  Make sure the
-      # sending side is sending ASCII data before you try to decode!
+      # And decode to ASCII text and print it too.
       packet_text = str(packet, "ascii")
       print("Received (ASCII): {0}".format(packet_text))
-      # Also read the RSSI (signal strength) of the last received message and
-      # print it.
+      # Also read the RSSI (signal strength) of the last received message and print it.
       rssi = rfm9x.last_rssi
       print("Received signal strength: {0} dB".format(rssi))
       # TODO: Decode radio packet with temp sensor information
