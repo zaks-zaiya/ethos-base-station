b05112bbfc2be80d3f6ce1dfaed7e17c4ed354d0
Connor Forbes <connorforbes25@gmail.com>
2023-02-06 17:05:22 +1000

    feat: Input box as passcode is being typed

--- javascript_ui/src/components/InputPasscode.vue
+++ javascript_ui/src/components/InputPasscode.vue
@@ -8,39 +8,101 @@
       click the "GO BACK" button</b
     >
 
+    <div class="row">
+      <q-input
+        filled
+        bottom-slots
+        class="full-width"
+        v-model="passcode"
+        label="passcode"
+        type="password"
+        counter
+        maxlength="4"
+      >
+        <template v-slot:append>
+          <q-btn round dense flat @click="backspacePasscode" icon="backspace" />
+        </template>
+      </q-input>
+    </div>
+
     <div class="row passcode-row">
       <div class="col q-pa-sm">
-        <q-btn class="full-width full-height" color="primary" label="1"></q-btn>
+        <q-btn
+          class="full-width full-height"
+          color="primary"
+          @click="appendPasscode('1')"
+          label="1"
+        ></q-btn>
       </div>
       <div class="col q-pa-sm">
-        <q-btn class="full-width full-height" color="primary" label="2"></q-btn>
+        <q-btn
+          class="full-width full-height"
+          color="primary"
+          @click="appendPasscode('2')"
+          label="2"
+        ></q-btn>
       </div>
       <div class="col q-pa-sm">
-        <q-btn class="full-width full-height" color="primary" label="3"></q-btn>
+        <q-btn
+          class="full-width full-height"
+          color="primary"
+          @click="appendPasscode('3')"
+          label="3"
+        ></q-btn>
       </div>
     </div>
 
     <div class="row passcode-row">
       <div class="col q-pa-sm">
-        <q-btn class="full-width full-height" color="primary" label="4"></q-btn>
+        <q-btn
+          class="full-width full-height"
+          color="primary"
+          @click="appendPasscode('4')"
+          label="4"
+        ></q-btn>
       </div>
       <div class="col q-pa-sm">
-        <q-btn class="full-width full-height" color="primary" label="5"></q-btn>
+        <q-btn
+          class="full-width full-height"
+          color="primary"
+          @click="appendPasscode('5')"
+          label="5"
+        ></q-btn>
       </div>
       <div class="col q-pa-sm">
-        <q-btn class="full-width full-height" color="primary" label="6"></q-btn>
+        <q-btn
+          class="full-width full-height"
+          color="primary"
+          @click="appendPasscode('6')"
+          label="6"
+        ></q-btn>
       </div>
     </div>
 
     <div class="row passcode-row">
       <div class="col q-pa-sm">
-        <q-btn class="full-width full-height" color="primary" label="7"></q-btn>
+        <q-btn
+          class="full-width full-height"
+          color="primary"
+          @click="appendPasscode('7')"
+          label="7"
+        ></q-btn>
       </div>
       <div class="col q-pa-sm">
-        <q-btn class="full-width full-height" color="primary" label="8"></q-btn>
+        <q-btn
+          class="full-width full-height"
+          color="primary"
+          @click="appendPasscode('8')"
+          label="8"
+        ></q-btn>
       </div>
       <div class="col q-pa-sm">
-        <q-btn class="full-width full-height" color="primary" label="9"></q-btn>
+        <q-btn
+          class="full-width full-height"
+          color="primary"
+          @click="appendPasscode('9')"
+          label="9"
+        ></q-btn>
       </div>
     </div>
 
@@ -54,7 +116,12 @@
         ></q-btn>
       </div>
       <div class="col q-pa-sm">
-        <q-btn class="full-width full-height" color="primary" label="0"></q-btn>
+        <q-btn
+          class="full-width full-height"
+          color="primary"
+          @click="appendPasscode('0')"
+          label="0"
+        ></q-btn>
       </div>
       <div class="col q-pa-sm">
         <q-btn
@@ -66,6 +133,28 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'InputPasscode',
+  setup() {
+    let passcode = ref('');
+
+    const backspacePasscode = () => {
+      // Remove last character of passcode
+      passcode.value = passcode.value.slice(0, -1);
+    };
+
+    const appendPasscode = (char: string) => {
+      passcode.value = passcode.value.concat(char);
+    };
+
+    return { passcode, backspacePasscode, appendPasscode };
+  },
+});
+</script>
 
 <style lang="scss" scoped>
 .passcode-row {
