4a966f58984464c12e9fe6f4e8aede9fac862ecb
Connor Forbes <connorforbes25@gmail.com>
2023-02-08 11:00:12 +1000

    feat: Passcode validation to input

--- javascript_ui/src/components/InputPasscode.vue
+++ javascript_ui/src/components/InputPasscode.vue
@@ -18,6 +18,8 @@
         type="password"
         counter
         maxlength="4"
+        :error="isIncorrectPasscode"
+        error-message="Incorrect passcode, try again"
       >
         <template v-slot:append>
           <q-btn round dense flat @click="backspacePasscode" icon="backspace" />
@@ -128,6 +130,7 @@
           class="full-width full-height"
           color="positive"
           label="Submit"
+          @click="checkPasscode"
         ></q-btn>
       </div>
     </div>
@@ -139,8 +142,9 @@ import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'InputPasscode',
-  setup() {
-    let passcode = ref('');
+  setup(props, { emit }) {
+    const passcode = ref('');
+    const isIncorrectPasscode = ref(false);
 
     const backspacePasscode = () => {
       // Remove last character of passcode
@@ -148,10 +152,32 @@ export default defineComponent({
     };
 
     const appendPasscode = (char: string) => {
+      // Append character to end of passcode
       passcode.value = passcode.value.concat(char);
     };
 
-    return { passcode, backspacePasscode, appendPasscode };
+    const checkPasscode = () => {
+      // Passcode is correct
+      if (passcode.value == process.env.SETTINGSPASSCODE) {
+        isIncorrectPasscode.value = false;
+        // Submit 'success' event to indicate passcode submitted successfully
+        emit('success');
+      }
+      // Passcode is incorrect
+      else {
+        // Display error and clear passcode
+        isIncorrectPasscode.value = true;
+        passcode.value = '';
+      }
+    };
+
+    return {
+      passcode,
+      isIncorrectPasscode,
+      backspacePasscode,
+      appendPasscode,
+      checkPasscode,
+    };
   },
 });
 </script>
