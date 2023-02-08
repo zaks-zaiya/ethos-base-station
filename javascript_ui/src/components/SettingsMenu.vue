9eb2583f680cd18a2f483b8a9c40c1a69f7f4314
Connor Forbes <connorforbes25@gmail.com>
2023-02-08 14:31:04 +1000

    feat: Basic sensor settings

--- javascript_ui/src/components/SettingsMenu.vue
+++ javascript_ui/src/components/SettingsMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="full-height">
+  <div>
     <q-splitter v-model="splitterModel" style="height: calc(100vh - 60px)">
       <template v-slot:before>
         <q-tabs v-model="tab" vertical class="text-teal">
@@ -13,17 +13,17 @@
           class="full-height q-mr-sm"
           v-model="tab"
           animated
-          swipeable
           vertical
           transition-prev="slide-down"
           transition-next="slide-up"
         >
           <q-tab-panel name="user-data">
             <div class="text-h4 q-mb-md">User Data</div>
+            <SettingsMenuUserData />
           </q-tab-panel>
 
           <q-tab-panel name="sensors">
-            <div class="text-h4 q-mb-md">Sensors</div>
+            <SettingsMenuSensors />
           </q-tab-panel>
         </q-tab-panels>
       </template>
@@ -32,14 +32,17 @@
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
+import { defineComponent, ref } from 'vue';
+import SettingsMenuSensors from './SettingsMenuSensors.vue';
+import SettingsMenuUserData from './SettingsMenuUserData.vue';
 
-export default {
+export default defineComponent({
+  components: { SettingsMenuUserData, SettingsMenuSensors },
   setup() {
     return {
       tab: ref('user-data'),
       splitterModel: ref(20),
     };
   },
-};
+});
 </script>
---
+++ javascript_ui/src/components/SettingsMenuSensors.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="q-pb-md">
+    <div v-for="(sensor, index) in allSensorData" :key="index">
+      <div class="text-h6">Sensor {{ index + 1 }}</div>
+      <q-separator />
+      <q-input
+        outlined
+        v-model="sensor.name"
+        label="Location"
+        class="q-my-md"
+      />
+      <q-input outlined v-model="sensor.id" label="ID" class="q-mb-lg" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { useSensorDataStore } from 'src/stores/sensorData';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const store = useSensorDataStore();
+
+    const allSensorData = store.allSensorData;
+
+    console.log(allSensorData);
+
+    return { allSensorData };
+  },
+});
+</script>
