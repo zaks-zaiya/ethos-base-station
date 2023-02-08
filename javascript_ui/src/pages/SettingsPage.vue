31fb5e0c6d4e011786f9f6d089d615431a708067
Connor Forbes <connorforbes25@gmail.com>
2023-02-08 11:16:21 +1000

    feat: Logic to display settingsmenu when password entered correctly

--- javascript_ui/src/pages/SettingsPage.vue
+++ javascript_ui/src/pages/SettingsPage.vue
@@ -1,15 +1,24 @@
 <template>
-  <InputPasscode />
+  <div>
+    <InputPasscode v-if="!isShowSettings" @success="isShowSettings = true" />
+    <SettingsMenu v-if="isShowSettings" />
+  </div>
 </template>
 
 <script lang="ts">
 import InputPasscode from 'src/components/InputPasscode.vue';
+import SettingsMenu from 'src/components/SettingsMenu.vue';
 
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { InputPasscode },
+  components: { InputPasscode, SettingsMenu },
+  setup() {
+    let isShowSettings = ref(false);
+
+    return { isShowSettings };
+  },
 });
 </script>
 
