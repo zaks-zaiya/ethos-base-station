7dd1ec9586e1b4f784721c1315940fdee42eb1ab
Connor Forbes <connorforbes25@gmail.com>
2023-02-22 11:48:31 +1000

    feat[userdata]: Check values before setting store

--- javascript_ui/src/components/SettingsMenuUserData.vue
+++ javascript_ui/src/components/SettingsMenuUserData.vue
@@ -3,8 +3,8 @@
     <div class="text-h6 q-mb-md">User Data</div>
     <q-input
       outlined
-      v-model.number="userDataStore.postcode"
-      :rules="[checkQldPostcode, findPostcode]"
+      v-model.number="newPostcode"
+      :rules="[checkQldPostcode, findAndSetPostcode]"
       :hint="`Lat: ${userDataStore.latitude}, Lon: ${userDataStore.longitude}`"
       type="number"
       label="Postcode"
@@ -13,21 +13,24 @@
 
     <q-input
       outlined
-      v-model.number="userDataStore.ageYears"
+      v-model.number="newAge"
+      :rules="[checkAndSetAge]"
       type="number"
       label="Age (years)"
       class="q-my-md"
     />
     <q-input
       outlined
-      v-model.number="userDataStore.heightCm"
+      v-model.number="newHeight"
+      :rules="[checkAndSetHeight]"
       type="number"
       label="Height (cm)"
       class="q-my-md"
     />
     <q-input
       outlined
-      v-model.number="userDataStore.weightKg"
+      v-model.number="newWeight"
+      :rules="[checkAndSetWeight]"
       type="number"
       label="Weight (kg)"
       class="q-my-md"
@@ -37,15 +40,20 @@
 
 <script lang="ts">
 import { useUserDataStore } from 'src/stores/userData';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 import postcodeArrayString from '../assets/australian_postcodes.js';
 
 export default defineComponent({
   setup() {
-    let userDataStore = useUserDataStore();
+    const userDataStore = useUserDataStore();
 
-    let checkQldPostcode = (postcode: number) => {
+    const newPostcode = ref(userDataStore.postcode);
+    const newAge = ref(userDataStore.ageYears);
+    const newHeight = ref(userDataStore.heightCm);
+    const newWeight = ref(userDataStore.weightKg);
+
+    const checkQldPostcode = (postcode: number) => {
       // QLD postcode should be from 4000-4999
       if (postcode >= 4000 && postcode < 5000) {
         return true;
@@ -53,25 +61,60 @@ export default defineComponent({
       return 'Please enter a QLD postcode (4000-4999)';
     };
 
-    let findPostcode = (postcode: number) => {
+    const findAndSetPostcode = (postcode: number) => {
       const postcodeArray = JSON.parse(postcodeArrayString);
       let postcodeString = postcode.toString();
       // Loop through all postcodes and find correct one
       for (let area of postcodeArray) {
+        // Postcode found
         if (area.postcode === postcodeString) {
-          // Set latitude and longitude
+          // Set postcode, latitude and longitude
+          userDataStore.postcode = postcode;
           userDataStore.latitude = area.lat;
           userDataStore.longitude = area.long;
           return true;
         }
       }
       // No postcode found
-      userDataStore.latitude = undefined;
-      userDataStore.longitude = undefined;
       return 'Postcode not found';
     };
 
-    return { userDataStore, checkQldPostcode, findPostcode };
+    const checkAndSetAge = (age: number) => {
+      if (age > 0 && age < 200) {
+        userDataStore.ageYears = age;
+        return true;
+      }
+      return 'Invalid age value';
+    };
+
+    const checkAndSetHeight = (height: number) => {
+      if (height > 30 && height < 300) {
+        userDataStore.heightCm = height;
+        return true;
+      }
+      return 'Invalid height value';
+    };
+
+    const checkAndSetWeight = (weight: number) => {
+      if (weight > 10 && weight < 300) {
+        userDataStore.weightKg = weight;
+        return true;
+      }
+      return 'Invalid weight value';
+    };
+
+    return {
+      userDataStore,
+      newPostcode,
+      newAge,
+      newHeight,
+      newWeight,
+      checkQldPostcode,
+      findAndSetPostcode,
+      checkAndSetAge,
+      checkAndSetHeight,
+      checkAndSetWeight,
+    };
   },
 });
 </script>
