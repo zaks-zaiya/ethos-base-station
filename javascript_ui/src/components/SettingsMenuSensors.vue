6ee81c9a9e2df876d876ff5b40d570a916e29fb1
Connor Forbes <connorforbes25@gmail.com>
2023-02-23 17:04:26 +1000

    feat: Verify sensor data before writing store

--- javascript_ui/src/components/SettingsMenuSensors.vue
+++ javascript_ui/src/components/SettingsMenuSensors.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- For each sensor -->
-    <div v-for="(sensor, index) in allSensorData" :key="index" class="q-mb-md">
+    <div v-for="(sensor, index) in newSensorData" :key="index" class="q-mb-md">
       <div class="row q-mb-md">
         <!-- Display sensor number -->
         <div class="col-2 text-bold">Sensor {{ index + 1 }}</div>
@@ -10,16 +10,14 @@
           <q-input
             outlined
             v-model="sensor.name"
-            :rules="[
-              (val) => (val.length > 0 ? true : 'Please enter sensor location'),
-            ]"
+            :rules="[(val) => checkAndSetSensorName(val, index)]"
             label="Location"
             class="q-mb-sm"
           />
           <q-input
             outlined
             v-model.number="sensor.id"
-            :rules="[checkSensorId]"
+            :rules="[(val) => checkAndSetSensorId(val, index)]"
             type="number"
             label="ID"
             hint="The ID number on the case"
@@ -34,24 +32,33 @@
 
 <script lang="ts">
 import { useSensorDataStore } from 'src/stores/sensorData';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   setup() {
-    const store = useSensorDataStore();
+    const allSensorData = useSensorDataStore().allSensorData;
+    // Get a deep copy of sensor data to prevent modification without verifying inputs
+    const newSensorData = ref(useSensorDataStore().getDeepCopySensorData);
 
-    const allSensorData = store.allSensorData;
-
-    const checkSensorId = (id: number) => {
-      if (id > 0 && id < 1000) {
+    const checkAndSetSensorName = (newName: string, index: number) => {
+      if (newName?.length > 0) {
+        allSensorData[index].name = newName;
         return true;
       }
-      return 'Enter valid ID (1-999)';
+      return 'Please enter sensor location';
     };
 
-    console.log(allSensorData);
+    const checkAndSetSensorId = (newId: number, index: number) => {
+      // Check if ID is valid
+      if (newId > 0 && newId < 1000) {
+        // Update original value
+        allSensorData[index].id = newId;
+        return true;
+      }
+      return 'Enter a valid ID (1-999)';
+    };
 
-    return { allSensorData, checkSensorId };
+    return { newSensorData, checkAndSetSensorName, checkAndSetSensorId };
   },
 });
 </script>
