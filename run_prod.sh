ac5c8b8f57ee2590e0c1bc9e2818f2ea2f2da958
Connor Forbes <connorforbes25@gmail.com>
2023-05-09 14:59:02 +1000

    fix: Build scripts & seperate build file

--- run_prod.sh
+++ run_prod.sh
@@ -1,10 +1,10 @@
 # Open quasar program in new window
 cd javascript_ui
-npm run build
 './dist/electron/Packaged/Ethos Heat Monitor-linux-armv7l/Ethos Heat Monitor' & # Ensure that process does not block python script
 cd ..
 
 # Run python
 cd python_radio
-source ./env/bin/activate
+# Use dot as alias for 'source'
+. ./env/bin/activate
 python3 src/main.py production
