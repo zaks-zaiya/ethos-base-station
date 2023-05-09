48c1eb7342c789c061b08f7c4611b31062b28f1a
Connor Forbes <connorforbes25@gmail.com>
2023-05-09 15:53:52 +1000

    feat: Hide mouse cursor and show feedback when clicked

--- javascript_ui/src/App.vue
+++ javascript_ui/src/App.vue
@@ -17,6 +17,48 @@ export default defineComponent({
 
     const forecastStore = useForecastStore();
     forecastStore.setup();
+
+    // Add touch/click feedback to app
+    document.addEventListener('click', function (event: MouseEvent) {
+      const effect = document.createElement('div');
+      effect.classList.add('click-effect');
+      effect.style.top = `${event.clientY - 25}px`; // offset by half the height/width to center the effect
+      effect.style.left = `${event.clientX - 25}px`;
+      document.body.appendChild(effect);
+
+      setTimeout(() => {
+        document.body.removeChild(effect);
+      }, 1000); // remove after 1 second
+    });
   },
 });
 </script>
+
+<style>
+/* Hide cursor in program for touch screen use */
+* {
+  cursor: none !important;
+}
+
+/* Click effect for haptic feedback */
+.click-effect {
+  position: absolute;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  background-color: rgba(0, 0, 0, 0.1);
+  pointer-events: none;
+  animation: click-effect 1s ease-out;
+}
+
+@keyframes click-effect {
+  from {
+    transform: scale(0);
+    opacity: 0.5;
+  }
+  to {
+    transform: scale(2);
+    opacity: 0;
+  }
+}
+</style>
