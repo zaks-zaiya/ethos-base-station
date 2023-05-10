1443730f8f8b8db8c205f6df3e768a1fa34565b8
Connor Forbes <connorforbes25@gmail.com>
2023-05-10 16:17:51 +1000

    refactor: Seperate radio init function

--- python_radio/src/main.py
+++ python_radio/src/main.py
@@ -14,6 +14,16 @@ try:
 except:
   print("Unable to import radio modules, are you on RPi?")
 
+def radio_init():
+  # Configure LoRa Radio
+  RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz
+  CS = DigitalInOut(board.CE1)
+  RESET = DigitalInOut(board.D25)
+  spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+  rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+  rfm9x.tx_power = 23
+  return rfm9x
+
 # Allow web (9000) or electron (9300) apps to connect
 sio = socketio.AsyncServer(cors_allowed_origins='*')
 app = web.Application()
@@ -30,13 +40,7 @@ def disconnect(sid):
 if __name__ == '__main__':
   production_arg = sys.argv[1] if len(sys.argv) > 1 else False
   if production_arg == 'prod' or production_arg == 'production':
-    # Configure LoRa Radio
-    RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz
-    CS = DigitalInOut(board.CE1)
-    RESET = DigitalInOut(board.D25)
-    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
-    rfm9x.tx_power = 23
+    rfm9x = radio_init()
     # Start radio listen thread
     radio_thread = threading.Thread(target=asyncio.run, args=(radio_listen(sio, rfm9x),))
     radio_thread.start()
