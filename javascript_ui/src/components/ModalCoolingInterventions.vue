e3e9bd52ef4c34f3845bbb30d2bacfa053b90647
Connor Forbes <connorforbes25@gmail.com>
2023-05-23 13:11:40 +1000

    skeleton: Modal heat alert

--- javascript_ui/src/components/ModalCoolingInterventions.vue
+++ javascript_ui/src/components/ModalCoolingInterventions.vue
@@ -50,7 +50,6 @@ export default defineComponent({
 
     return {
       showModal,
-      isTemperatureRisk: computed(() => true),
       displayInfo,
       showInfo,
     };
---
+++ javascript_ui/src/components/ModalHeatAlert.vue
@@ -0,0 +1,42 @@
+<!-- ModalNoConnection.vue -->
+<template>
+  <q-dialog v-model="showModal" full-width persistent> </q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs, computed } from 'vue';
+
+export default defineComponent({
+  name: 'ModalCoolingInterventions',
+  components: {},
+  emits: ['update:modelValue'],
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup(props, { emit }) {
+    const { modelValue } = toRefs(props);
+
+    const showModal = computed({
+      get() {
+        return modelValue.value;
+      },
+      set(newValue) {
+        emit('update:modelValue', newValue);
+      },
+    });
+
+    return {
+      showModal,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.cooling-intervention {
+  height: calc(100vh / 3);
+}
+</style>
