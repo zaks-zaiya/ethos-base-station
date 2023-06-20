b5ee1d197b12e2ff24261eb51b187383af9243b8
Connor Forbes <connorforbes25@gmail.com>
2023-06-20 12:21:28 +1000

    devops: Added js testing to test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,14 @@
+# RUN JS TESTS
+cd javascript_ui
+echo "-----\nTesting Javascript UI:\n-----"
+npm run test:unit:ci
+cd ..
+
+# RUN PYTHON TESTS
 cd python_radio
 . ./env/bin/activate
 cd src
 # Find and run all tests inside `tests` directory
+echo "-----\nTesting Python Radio Module:\n-----"
 python3 -m unittest discover tests -b
 cd ../..
