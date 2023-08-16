63936d513b968b6a478974220fa1da2d346c008d
Connor Forbes <connorforbes25@gmail.com>
2023-08-16 13:15:02 +1000

    refactor: [forecast] Improved structure of posting to DB

--- javascript_ui/src/stores/forecast.ts
+++ javascript_ui/src/stores/forecast.ts
@@ -26,6 +26,14 @@ export const useForecastStore = defineStore('forecast', {
   getters: {},
 
   actions: {
+    postToDatabase() {
+      const databaseStore = useDatabaseStore();
+      databaseStore.postDocument('weather', {
+        weatherLocation: this.stationName,
+        temperature: this.currentTemp,
+        humidity: this.currentHumidity,
+      });
+    },
     setup() {
       // Initialize user data for lat/lon
       const dataUserStore = useDataUserStore();
@@ -84,12 +92,7 @@ export const useForecastStore = defineStore('forecast', {
         // Clear error message
         this.errorMessage = '';
         // Post data to database
-        const databaseStore = useDatabaseStore();
-        databaseStore.postDocument('weather', {
-          weatherLocation: this.stationName,
-          temperature: this.currentTemp,
-          humidity: this.currentHumidity,
-        });
+        this.postToDatabase();
       } catch (error: unknown) {
         if (error instanceof AxiosError) {
           this.errorMessage = error.message;
