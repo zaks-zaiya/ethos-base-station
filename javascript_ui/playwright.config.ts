49dd065bff207536fe679e2c9b60f3fafe8e5d8b
Connor Forbes <connorforbes25@gmail.com>
2023-08-30 17:01:28 +1000

    chore: Move playwright output into playwright folder

--- javascript_ui/playwright.config.ts
+++ javascript_ui/playwright.config.ts
@@ -11,6 +11,9 @@ require('dotenv').config();
  */
 export default defineConfig({
   testDir: './test/playwright',
+  outputDir: './test/playwright/output',
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [['html', { outputFolder: './test/playwright/playwright-report' }]],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -19,8 +22,6 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
