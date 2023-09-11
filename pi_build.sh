ff0bb9e9450b396d91db4c9a06084a6fe0cbd9d2
Connor Forbes <connorforbes25@gmail.com>
2023-09-11 16:20:04 +1000

    feat: Update scripts to 64 bit build

--- pi_build.sh
+++ pi_build.sh
@@ -3,5 +3,6 @@ cd javascript_ui
 yarn install
 yarn build
 # Make built electron app executable
-chmod +x "dist/electron/Packaged/Ethos Heat Monitor-linux-armv7l/Ethos Heat Monitor"
+# chmod +x "dist/electron/Packaged/Ethos Heat Monitor-linux-armv7l/Ethos Heat Monitor"
+chmod +x "dist/electron/Packaged/Ethos Heat Monitor-linux-armv64/Ethos Heat Monitor"
 cd ..
--- pi_run_manual.sh
+++ pi_run_manual.sh
@@ -1,7 +1,8 @@
 # cd javascript_ui
 # Need to use absolute path to work from rc.local for startup
 cd ~/ethos-raspberry-pi/javascript_ui
-'./dist/electron/Packaged/Ethos Heat Monitor-linux-armv7l/Ethos Heat Monitor' & # Ensure that process does not block python script
+# './dist/electron/Packaged/Ethos Heat Monitor-linux-armv7l/Ethos Heat Monitor' & # Ensure that process does not block python script
+'./dist/electron/Packaged/Ethos Heat Monitor-linux-armv64/Ethos Heat Monitor' & # Ensure that process does not block python script
 cd ..
 
 # Run python
