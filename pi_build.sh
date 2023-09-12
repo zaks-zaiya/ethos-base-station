c3ab1cc051e792d25110af43d5df82ea79f26657
Connor Forbes <connorforbes25@gmail.com>
2023-09-12 14:49:30 +1000

    feat: Add fallback to build script incase it fails

--- pi_build.sh
+++ pi_build.sh
@@ -3,10 +3,28 @@
 export NVM_DIR="$HOME/.config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Build electron App
+# Navigate to javascript directory
 cd javascript_ui
+
+# Backup the dist directory
+if [ -d "dist" ]; then
+  echo "Creating backup of dist directory"
+  cp -r dist dist_backup || { echo "Failed to create backup of dist directory"; exit 1; }
+fi
+
+# Build electron App
 yarn install
-yarn build
+# If yarn build fails, restore dist from backup and exit
+yarn build || {
+  echo "Yarn build failed, restoring backup";
+  rm -rf dist && cp -r dist_backup dist;
+}
+
+# Remove the backup folder
+if [ -d "dist_backup" ]; then
+  rm -rf dist_backup
+fi
+
 # Make built electron app executable
 # chmod +x "dist/electron/Packaged/Ethos Heat Monitor-linux-armv7l/Ethos Heat Monitor"
 chmod +x "dist/electron/Packaged/Ethos Heat Monitor-linux-arm64/Ethos Heat Monitor"
