09b02c15a795607cd532d2ce5d0789dc577888cc
Connor Forbes <connorforbes25@gmail.com>
2023-10-26 14:46:31 +1000

    chore: Change fan thresholds based on new research

--- javascript_ui/src/helpers/fanAndWindowUse.ts
+++ javascript_ui/src/helpers/fanAndWindowUse.ts
@@ -4,21 +4,13 @@ export const shouldUseFan = (
   sensor: SensorData
 ): 'yes' | 'maybe' | 'no' | undefined => {
   if (sensor.humidity && sensor.temperature) {
-    /* Based on Figure 1 (healthy older people)
-     * https://www.thelancet.com/journals/lanplh/article/PIIS2542-5196(21)00136-4/fulltext
+    /* Based on table 1 from here:
+     * https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(21)01209-5/fulltext
      */
-    const x = sensor.humidity;
-    const fanThreshold =
-      -3.24 * Math.pow(x, 4) * Math.pow(10, -7) +
-      8.4 * Math.pow(x, 3) * Math.pow(10, -5) -
-      7.86 * Math.pow(x, 2) * Math.pow(10, -3) +
-      2.43 * Math.pow(x, 1) * Math.pow(10, -1) +
-      37.93;
-
-    if (sensor.temperature < fanThreshold - 1) {
+    if (sensor.temperature < 37) {
       // Safe to use fan
       return 'yes';
-    } else if (sensor.temperature < fanThreshold + 1) {
+    } else if (sensor.temperature < 38) {
       // Can maybe use fan
       return 'maybe';
     } else {
