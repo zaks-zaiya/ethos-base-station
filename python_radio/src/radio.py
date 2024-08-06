cfdf061ec20516092ca52a63bc25f1be611d8b1c
Connor Forbes <connorforbes25@gmail.com>
2024-08-06 19:11:38 +1000

    feat: Use asyncio instead of seperate thread

--- python_radio/src/radio.py
+++ python_radio/src/radio.py
@@ -2,34 +2,28 @@
 from adafruit_rfm9x import RFM9x
 from typing import Union
 from logger import Logger
-import threading
 import asyncio
-# For unpacking binary data
 import struct
 
-# For typing stop_event
-from threading import Event
-
-from encryption import Encryption  # Import the Encryption class
+from encryption import Encryption
 
 # Define class instance
 aesEncryption = Encryption()
 
-async def radio_listen(rfm9x: RFM9x, stop_event: Event, callback_function):
+async def radio_listen(rfm9x: RFM9x, stop_event: asyncio.Event, callback_function):
   while not stop_event.is_set():
     try:
       radio_packet = rfm9x.receive(timeout=5.0)
 
       if radio_packet is not None:
-        # Create and start a new thread to handle the processing
-        thread = threading.Thread(target=process_and_callback, args=(radio_packet, rfm9x.last_rssi, callback_function))
-        thread.start()
+        # Process and callback asynchronously
+        asyncio.create_task(process_and_callback(radio_packet, rfm9x.last_rssi, callback_function))
 
     except Exception as e:
       Logger.error(f"Error receiving packet: {e}")
-      continue
+      await asyncio.sleep(1)  # Add a small delay to avoid tight loop
 
-def process_and_callback(radio_packet, rssi, callback_function):
+async def process_and_callback(radio_packet, rssi, callback_function):
   # Decrypt received radio data
   try:
     decrypted_packet = aesEncryption.decrypt(radio_packet)
@@ -52,9 +46,9 @@ def process_and_callback(radio_packet, rssi, callback_function):
 
   # Call the callback function with the processed data
   try:
-    asyncio.run(callback_function(radio_data))
+    await callback_function(radio_data)
   except Exception as e:
-      Logger.error(f"Error in callback function: {e}")
+    Logger.error(f"Error in callback function: {e}")
 
 def process_packet(packet: bytearray, rssi: Union[float, int]):
   try:
